ec468033c1dd3aa8895de9a012a4da29e2d6e015
Karthick Mani <karthick_mani@apple.com>
2023-10-11 20:05:07 -0700

    implementing streamlit app

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
+import requests
+from io import BytesIO
 
 st.title('Ranveer Brar Recipes from YouTube')
+df = pd.read_excel('data/recipe_with_instructions.xlsx')
+
+recipe_selection = st.selectbox("Search for recipes", df['title'].drop_duplicates(), placeholder="Search recipes...", index=None)
+
+if recipe_selection:
+    df_filtered = df[df['title']==recipe_selection]
+else:
+    df_filtered = df
+
+st.subheader(df_filtered['title'][0])
+st.image(Image.open(BytesIO(requests.get(df_filtered['thumbnail'][0]).content)))
+st.markdown(df_filtered['ingredients_recipe'][0])
