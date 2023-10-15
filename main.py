7f0ea932bda4cb165a55e22a4c13e7ff30f02c17
Karthick Mani <karthickmani@MacBook-Air-2.attlocal.net>
2023-10-15 11:13:12 -0700

    prompt change

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 from io import BytesIO
 
 st.title('Ranveer Brar Recipes from YouTube')
-df = pd.read_excel('data/recipe_with_instructions.xlsx')
+df = pd.read_csv('data/recipe_with_instructions.csv')
 
 recipe_selection = st.selectbox("Choose a recipe from below to see ingredients and instructions", df['title'].drop_duplicates(), placeholder="Search recipes...", index=None)
 
@@ -15,7 +15,10 @@ if recipe_selection:
     st.subheader(df_filtered['title'].values[0])
     st.markdown("[YouTube Link](https://www.youtube.com/watch?v={})".format(df_filtered['video_id'].values[0]))
     st.image(Image.open(BytesIO(requests.get(df_filtered['thumbnail'].values[0]).content)))
-    st.markdown(df_filtered['ingredients_recipe'].values[0])
+    st.markdown('### Ingredients')
+    st.markdown(df_filtered['ingredients'].values[0])
+    st.markdown('### Recipe')
+    st.markdown(df_filtered['recipe'].values[0])
 else:
     df_filtered = df
 
